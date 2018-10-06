4a0c93d5e0e08349fa3c62896340ea8bb05a45ff
kamenim <kamenim@gmail.com>
2018-10-06 12:31:32 +0300

    mods: Servers, modules, channels + vendors :)

--- cmd/k8s-workshop/main.go
+++ cmd/k8s-workshop/main.go
@@ -1,15 +1,64 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/kamenim/k8s-workshop/internal/diagnostics"
 )
 
 func main() {
-	log.Print("simple hello")
+	log.Print("Simple hello")
 
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
+	srvPort := os.Getenv("PORT")
+	if len(srvPort) == 0 {
+		srvPort = "8080"
+	}
+	diagPort := os.Getenv("DIAG_PORT")
+	if len(diagPort) == 0 {
+		diagPort = "8585"
+	}
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", hello)
+
+	possibleErrors := make(chan error, 2)
+
+	go func() {
+		server := &http.Server{
+			Addr:    ":" + srvPort,
+			Handler: router,
+		}
+		err := server.ListenAndServe()
+		if err != nil {
+			possibleErrors <- err
+		}
+	}()
+
+	go func() {
+		diagnostics := diagnostics.NewDiagnostics()
+		diagServer := &http.Server{
+			Addr:    ":" + diagPort,
+			Handler: diagnostics,
+		}
+		err := diagServer.ListenAndServe()
+		// err := http.ListenAndServe(":"+diagPort, diagnostics)
+		if err != nil {
+			// log.Fatal(err)
+			possibleErrors <- err
+		}
+	}()
+
+	// block on channel
+	select {
+	case err := <-possibleErrors:
 		log.Fatal(err)
 	}
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, http.StatusText(http.StatusOK))
 }
---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/kamenim/k8s-workshop
+
+require (
+	github.com/gorilla/context v1.1.1 // indirect
+	github.com/gorilla/mux v1.6.2
+)
